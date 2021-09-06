84ae57763648c05a75cecbe9a75a919b8c49b701
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-09-06 01:58:59 -0500

    changed the message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h1>Hello World from AWS!</h1>');
 });
 
 app.listen(3000, () => {
